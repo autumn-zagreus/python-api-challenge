7bc33605c7f6e97b194d93bd1ee2ed6c4dfc750e
Gautam Iyer <97266373+autumn-zagreus@users.noreply.github.com>
2022-07-26 18:39:44 -0400

    got some done
    Worked on WeatherPy

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "ace29d393ff83b7c6b601b0c3980a3c3"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBoPdwA-bt5qU9B4WXcVvjmU5bQIzZZ6V0"
